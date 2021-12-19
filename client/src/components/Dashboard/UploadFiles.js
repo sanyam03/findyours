04aa8c611941c419c6c051a6630f6cc97fc21052
Emissa <temiline@gmail.com>
2021-12-19 00:36:08 -0800

    fix: report form styling
    feat: add logo to nav bar, click on this opens the report form if user is authenticated, otherwise it directs user to /login route
    
    feat: work on more styling

--- client/src/components/Dashboard/UploadFiles.js
+++ client/src/components/Dashboard/UploadFiles.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import UploadService from "./FileUploadService";
-import Button from "@material-ui/core/Button";
-import axios from "../../utils/axios";
 import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import UploadService from "./FileUploadService";
+import axios from "../../utils/axios";
 import './UploadFiles.css';
 
 const UploadFiles = (props) => {
@@ -20,18 +20,18 @@ const UploadFiles = (props) => {
       setFileInfos(response.data);
     });
   }, []);
-  
+
   const selectFile = (event) => {
     setSelectedFiles(event.target.files);
   };
   const handleSubmit = (event) => {
-    
+
     axios
       .get("/user/api/matchface")
       .then((result) => {
         console.log(result)
         debugger
-       
+
       })
       .catch((err) => {
         console.log(err.response)
@@ -77,90 +77,75 @@ const UploadFiles = (props) => {
 
   return (
     <div className={`popup ${props.isOpen ? "popup_open" : ""}`}>
-      <form 
+      <form
         action="#"
         noValidate
         className="form missing-cases"
       >
         <div className="form__container">
-          <h3 className="form__title">Enter Informations below to Search for the Missing Person</h3>
+          <h3 className="form__title">Search for the Missing Person</h3>
 
           <fieldset className="form__field">
-            <label className="form__label">Your Name</label>
-            <input
-              id="username"
-              type="text"
-              name="username"
-              placeholder="Enter your Name"
-              className="form__input form__input_type_username"
-              required
-            />
-            <span
-              id="username-error"
-              className="form__error form__error_active"
-            >
-            </span>
-
+            <div className="form__field_content">
+              <label className="form__label">Your Name</label>
+              <input
+                id="username"
+                type="text"
+                name="username"
+                placeholder="Enter your Name"
+                className="form__input form__input_type_username"
+                required
+              />
+              <span id="username-error" className="form__error form__error_active"></span>
+            </div>
             <h2 className='form__subtitle'>Missing Person's Identity</h2>
-            
-            <label className="form__label">Name</label>
-            <input
-              id="casename"
-              type="text"
-              name="casename"
-              placeholder="Enter Missing Person's Name"
-              className="form__input form__input_type_username"
-              required
-            />
-            <span
-              id="casename-error"
-              className="form__error form__error_active"
-            >
-            </span>
-
-            <label className="form__label">Age</label>
-            <input
-              id="age"
-              type="text"
-              name="age"
-              placeholder="Enter Missing Person's Age"
-              className="form__input form__input_type_age"
-            />
-            <span
-              id="age-error"
-              className="form__error form__error_active"
-            >
-            </span>
-
-            <label className="form__label">Phone</label>
-            <input
-              id="phone"
-              type="text"
-              name="phone"
-              placeholder="Enter Phone number"
-              className="form__input form__input_type_phone"
-              required
-            />
-            <span
-              id="age-error"
-              className="form__error form__error_active"
-            >
-            </span>
-
-            <label className="form__label">Father's Name</label>
-            <input
-              id="fathername"
-              type="text"
-              name="fathername"
-              placeholder="Enter their Father's Name"
-              className="form__input form__input_type_fathername"
-              required
-            />
-            <span
-              id="fathername-error"
-              className="form__error form__error_active"
-            >
-            </span>
+            <div className="form__field_content">
+              <label className="form__label">Name</label>
+              <input
+                id="casename"
+                type="text"
+                name="casename"
+                placeholder="Enter Missing Person's Name"
+                className="form__input form__input_type_username"
+                required
+              />
+              <span id="casename-error" className="form__error form__error_active"></span>
+            </div>
+            <div className="form__field_content">
+              <label className="form__label">Age</label>
+              <input
+                id="age"
+                type="number"
+                name="age"
+                placeholder="Enter Missing Person's Age"
+                className="form__input form__input_type_age"
+              />
+              <span id="age-error" className="form__error form__error_active"></span>
+            </div>
+            <div className="form__field_content">
+              <label className="form__label">Phone</label>
+              <input
+                id="phone"
+                type="number"
+                name="phone"
+                placeholder="Enter Phone number"
+                className="form__input form__input_type_phone"
+                required
+              />
+              <span id="phone-error" className="form__error form__error_active"></span>
+            </div>
+            <div className="form__field_content">
+              <label className="form__label">Father's Name</label>
+              <input
+                id="fathername"
+                type="text"
+                name="fathername"
+                placeholder="Enter their Father's Name"
+                className="form__input form__input_type_fathername"
+                required
+              />
+              <span id="fathername-error" className="form__error form__error_active"></span>
+            </div>
           </fieldset>
 
           {currentFile && (
@@ -213,11 +198,13 @@ const UploadFiles = (props) => {
               color: "white",
               margin: "0 auto",
             }}
-          onClick={handleSubmit} 
+          onClick={handleSubmit}
           >
             Submit
           </Button>
         </div>
+
+        <button type="button" aria-label="close" className="form__close" onClick={props.onClose}></button>
       </form>
     </div>
   );
